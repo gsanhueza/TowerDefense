3a661ca86f8434f2aabbe49ba30c8a79d966330d
gsanhueza <gabriel_8032@hotmail.com>
2017-04-12 17:52:45 -0300

    Tutorial 1 implementado

--- TutorialApplication.cpp
+++ TutorialApplication.cpp
@@ -4,12 +4,12 @@ Filename:    TutorialApplication.cpp
 -----------------------------------------------------------------------------
 
 This source file is part of the
-   ___                 __    __ _ _    _ 
+   ___                 __    __ _ _    _
   /___\__ _ _ __ ___  / / /\ \ (_) | _(_)
  //  // _` | '__/ _ \ \ \/  \/ / | |/ / |
 / \_// (_| | | |  __/  \  /\  /| |   <| |
 \___/ \__, |_|  \___|   \/  \/ |_|_|\_\_|
-      |___/                              
+      |___/
       Tutorial Framework
       http://www.ogre3d.org/tikiwiki/
 -----------------------------------------------------------------------------
@@ -28,7 +28,43 @@ TutorialApplication::~TutorialApplication(void)
 //-------------------------------------------------------------------------------------
 void TutorialApplication::createScene(void)
 {
-    // create your scene here :)
+    // Set camera as first position
+    mCamera->setPosition(0, 47, 222);
+
+    // Ambient Light
+    mSceneMgr->setAmbientLight(Ogre::ColourValue(0.5, 0.5, 0.5));
+
+    // Ogre Entity
+    Ogre::Entity* ogreEntity = mSceneMgr->createEntity("ogrehead.mesh");
+
+    Ogre::SceneNode* ogreNode = mSceneMgr->getRootSceneNode()->createChildSceneNode();
+    ogreNode->attachObject(ogreEntity);
+
+    // Ogre Entity 2 (Copy in different position)
+    Ogre::Entity* ogreEntity2 = mSceneMgr->createEntity("ogrehead.mesh");
+
+    Ogre::SceneNode* ogreNode2 = mSceneMgr->getRootSceneNode()->createChildSceneNode(Ogre::Vector3(84, 48, 0));
+    ogreNode2->attachObject(ogreEntity2);
+
+    // Ogre Entity 3 (Deformed)
+    Ogre::Entity* ogreEntity3 = mSceneMgr->createEntity("ogrehead.mesh");
+
+    Ogre::SceneNode* ogreNode3 = mSceneMgr->getRootSceneNode()->createChildSceneNode();
+    ogreNode3->setPosition(0, 104, 0);
+    ogreNode3->setScale(2, 1.2, 1);
+    ogreNode3->attachObject(ogreEntity3);
+
+    // Ogre Entity 4 (Rotated)
+    Ogre::Entity* ogreEntity4 = mSceneMgr->createEntity("ogrehead.mesh");
+
+    Ogre::SceneNode* ogreNode4 = mSceneMgr->getRootSceneNode()->createChildSceneNode();
+    ogreNode4->setPosition(-84, 48, 0);
+    ogreNode4->roll(Ogre::Degree(-90));
+    ogreNode4->attachObject(ogreEntity4);
+
+    // Illumination
+    Ogre::Light* light = mSceneMgr->createLight("MainLight");
+    light->setPosition(20, 80, 50);
 }
 
 
